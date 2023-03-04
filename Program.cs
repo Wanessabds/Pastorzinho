8373a92db73ad9a94b968be23dfd43ed69c92d13
Wanessabds <wanessa.nessap@gmail.com>
2023-03-03 22:16:14 -0300

    OPastorzinho

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("--- O Pastorzinho ---");
+Console.WriteLine("dó, ré, mi, fá, fá, fá,");
+Console.WriteLine("dó, ré, dó, ré, ré, ré,");
+Console.WriteLine("dó, sol, fá, mi, mi, mi,");
+Console.WriteLine("dó, ré, mi, fá, fá, fá.");
